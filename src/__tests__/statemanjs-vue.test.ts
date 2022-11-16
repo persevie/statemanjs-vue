00e552b79080722c3d041f11c34a47d042dac642
Dmitry Shatokhin <dmtrshatokhin@gmail.com>
2022-11-16 21:55:34 +0300

    chore: release statemanjs-vue

---
+++ src/__tests__/statemanjs-vue.test.ts
@@ -0,0 +1,72 @@
+import { mount } from "@vue/test-utils";
+import { useStatemanjs } from "../index";
+import { createState } from "@persevie/statemanjs";
+
+test("it should rerender after click and show the new state value", async () => {
+    const countState = createState(0);
+    const Counter = {
+        template: `
+      <div>
+        <button @click="increment" />
+        <p>Count: {{ count }}</p>
+      </div>
+    `,
+        setup() {
+            const count = useStatemanjs(countState);
+
+            return {
+                count,
+            };
+        },
+        methods: {
+            increment() {
+                countState.set(countState.get() + 1);
+            },
+        },
+    };
+
+    const wrapper = mount(Counter);
+
+    expect(wrapper.find("p").text()).toBe("Count: 0");
+
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.find("p").text()).toBe("Count: 1");
+
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.find("p").text()).toBe("Count: 2");
+});
+
+test("it should only rerender if the state value is 2", async () => {
+    const countState = createState(0);
+    const Counter = {
+        template: `
+      <div>
+        <button @click="increment" />
+        <p>Count: {{ count }}</p>
+      </div>
+    `,
+        setup() {
+            const count = useStatemanjs(countState, {
+                notifyCondition: (state) => state == 2,
+            });
+
+            return {
+                count,
+            };
+        },
+        methods: {
+            increment() {
+                countState.set(countState.get() + 1);
+            },
+        },
+    };
+
+    const wrapper = mount(Counter);
+
+    expect(wrapper.find("p").text()).toBe("Count: 0");
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.find("p").text()).toBe("Count: 0");
+
+    await wrapper.find("button").trigger("click");
+    expect(wrapper.find("p").text()).toBe("Count: 2");
+});
---
+++ src/useStatemanjs.ts
@@ -0,0 +1,31 @@
+import { getCurrentScope, onScopeDispose, ShallowRef, shallowRef } from "vue";
+import type { StatemanjsAPI, SubscriptionOptions } from "@persevie/statemanjs";
+
+/**
+ * Make the vue component observe the statemanjs.
+ * In the options, you can specify information about the observer,
+ * as well as specify the condition under which the observer will be notified.
+ *
+ * @param statemanjs Statemanjs to observe.
+ * @param subscriptionOptions Additional information and notification condition.
+ * @returns Reactive state.
+ */
+function useStatemanjs<T>(
+    statemanjs: StatemanjsAPI<T>,
+    subscriptionOptions: SubscriptionOptions<T> = {},
+): ShallowRef<T> {
+    const state = shallowRef();
+    state.value = statemanjs.get();
+
+    if (getCurrentScope()) {
+        const unsubscribe = statemanjs.subscribe((value) => {
+            state.value = value;
+        }, subscriptionOptions as any);
+
+        onScopeDispose(unsubscribe);
+    }
+
+    return state;
+}
+
+export { useStatemanjs };
